b4f65cac3207357de4b7c57e2c335cd52d130198
mrpiggy97 <fabyjesusrivas10@gmail.com>
2022-02-24 12:51:12 +0000

    deleted waiter

--- main.go
+++ main.go
@@ -1,14 +1,7 @@
 package main
 
-import (
-	"sync"
-
-	"github.com/mrpiggy97/secondAwsTest/servers"
-)
+import "github.com/mrpiggy97/secondAwsTest/servers"
 
 func main() {
-	var waiter *sync.WaitGroup = new(sync.WaitGroup)
-	waiter.Add(1)
-	servers.RunServers()
-	waiter.Wait()
+	servers.RunServer()
 }
